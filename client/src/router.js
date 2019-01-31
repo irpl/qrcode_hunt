4f06bda30b7f01507c9710b23829933939a1aace
Phillip <philo3194@gmail.com>
2019-01-31 11:20:21 -0500

    added a make route to accept new games, not done yet

--- client/src/router.js
+++ client/src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Quest from './views/Quest.vue'
+import Make from './views/Make.vue'
 
 Vue.use(Router)
 
@@ -16,6 +17,11 @@ export default new Router({
       path: '/quest',
       name: 'quest',
       component: Quest
+    },
+    {
+      path: '/make',
+      name: 'make',
+      component: Make
     }
   ]
 })
---
+++ client/src/views/Make.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <div class="container">
+      <form>
+        <div class="make-input-div">
+          <input class="make-input" type="text" v-model="event" placeholder="Game name"/>
+        </div>
+        <div class="make-input-div">
+          <input class="make-input" type="text" v-model="gameMaker" placeholder="Your name"/>
+        </div>
+        <div v-for="(quests, index) in quests" :key="index">
+          Title: <input type="text" v-model="quests.title" /> 
+          Clue: <textarea v-model="quests.clue" />
+          <button @click="deleteRow(index)">Delete</button>
+        </div>
+        <button class="btn btn-lg" @click="addRow">Add row</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      quests: [],
+      gameMaker: '',
+      event: ''
+    }
+  },
+  methods: {
+    addRow(e) {
+      e.preventDefault();
+      
+      this.quests.push({
+        title: '',
+        clue: ''
+      })
+    },
+    deleteRow(index) {
+      this.inputs.splice(index,1)
+    }
+  }
+}
+</script>
+
+<style>
+.make-input-div {
+    position: relative;
+    display: flex;
+    flex-direction: row;
+    /* width: 100%; */
+    max-width: 300px;
+    margin: 15px auto;
+    border-radius: 2px;
+    padding: 1.4rem 2rem 1.6rem;
+    background: #2a2826;
+}
+.make-input-div input {
+    flex-grow: 1;
+    width: 100%;
+    color: white;
+    font-size: 1.8rem;
+    line-height: 2.4rem;
+    vertical-align: middle;
+}
+.make-input {
+    border-style: none;
+    background: transparent;
+    outline: none;
+}
+</style>
