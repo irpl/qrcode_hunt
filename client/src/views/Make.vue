2aa402ecd0e006e2f996f4f52e4de0057d51dab7
Phillip <philo3194@gmail.com>
2019-02-03 10:42:26 -0500

    i have no idea, i guess i jus basically finished writing the func

--- client/src/views/Make.vue
+++ client/src/views/Make.vue
@@ -1,31 +1,47 @@
 <template>
   <div>
-    <div class="container">
-      <form>
+    <Made v-if="success" v-bind:game="$data"/>
+    <div class="container" v-else>
+      <h1>Create your own game</h1>
+      <!-- <form> -->
+        <h2>General</h2>
         <div class="make-input-div">
           <input class="make-input" type="text" v-model="event" placeholder="Game name"/>
         </div>
         <div class="make-input-div">
           <input class="make-input" type="text" v-model="gameMaker" placeholder="Your name"/>
         </div>
+        <hr>
+        <h2>Quests</h2>
         <div v-for="(quests, index) in quests" :key="index">
-          Title: <input type="text" v-model="quests.title" /> 
-          Clue: <textarea v-model="quests.clue" />
-          <button @click="deleteRow(index)">Delete</button>
+          <div class="make-input-div">
+            <input class="make-input" type="text" v-model="quests.title" placeholder="Title"/>
+          </div>
+          <div class="make-input-div">
+            <textarea class="make-input" v-model="quests.clue" placeholder="Clue"/>
+          </div>
+          <button class="btn btn-sm del" @click="deleteRow(index)">- del</button>
         </div>
-        <button class="btn btn-lg" @click="addRow">Add row</button>
-      </form>
+        <button class="btn btn-sm add" @click="addRow"><span>+ </span> add</button>
+        <button class="btn btn-lg" @click="submit">Submit</button>
+      <!-- </form> -->
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+import Made from '../components/Made.vue'
 export default {
+  components: {
+    Made
+  },
   data () {
     return {
       quests: [],
       gameMaker: '',
-      event: ''
+      event: '',
+      success: false
     }
   },
   methods: {
@@ -38,13 +54,31 @@ export default {
       })
     },
     deleteRow(index) {
-      this.inputs.splice(index,1)
+      // e.preventDefault();
+      this.quests.splice(index,1)
+    },
+    submit () {
+      axios.post('/game', this.$data)
+        .then(res => {
+          if (res.data) {
+            this.success = res.data.success
+          } else {
+            alert("There was an error")
+          }
+        });
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.btn-sm.del {
+  margin: -6px 6px;
+}
+.btn-sm.add {
+  float: right;
+  margin-bottom: 40px;
+}
 .make-input-div {
     position: relative;
     display: flex;
@@ -53,16 +87,22 @@ export default {
     max-width: 300px;
     margin: 15px auto;
     border-radius: 2px;
-    padding: 1.4rem 2rem 1.6rem;
+    padding: 10px 20px;
     background: #2a2826;
 }
 .make-input-div input {
     flex-grow: 1;
     width: 100%;
     color: white;
-    font-size: 1.8rem;
+    font-size: 20px;
     line-height: 2.4rem;
     vertical-align: middle;
+}
+.make-input-div textarea {
+  color: inherit;
+  width: 100%;
+  font-size: 20px;
+  height: 3em;
 }
 .make-input {
     border-style: none;
