13e0436db9691ea996e10d76ef05c91a75a05d0f
Phillip <philo3194@gmail.com>
2019-02-03 10:40:26 -0500

    added the made component

---
+++ client/src/components/Made.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <h1>Your game has been made</h1>
+
+    <p>You'll use this QRCode to start your game.</p>
+    <div class="made-qr">
+      <img v-bind:src="'https://api.qrserver.com/v1/create-qr-code/?size=300x300&margin=35&data='+game.event" />
+    </div>
+
+    <p>Print and hide these QRCodes for your friends to find.</p>
+    <div class="made-qr" v-for="(quest, index) in game.quests" :key="index">
+      <img v-bind:src="'https://api.qrserver.com/v1/create-qr-code/?size=300x300&margin=35&data='+quest._id" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Made",
+  props: ["game"],
+}
+</script>
+
+<style>
+.made-qr {
+  width: 45%;
+  display: inline-block;
+  padding: 2.5% 2.5%;
+}
+
+.made-qr > img {
+  width: 100%;
+}
+</style>
