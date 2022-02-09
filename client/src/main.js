86b4723ad98b21c3548349007ec39c82472080f5
Phillip Logan <philo3194@gmail.com>
2022-02-09 22:11:26 +0000

    trying

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import axios from 'axios'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
 
-import VueQrcodeReader from 'vue-qrcode-reader'
+import VueQrcodeReader from "vue-qrcode-reader";
 
-Vue.use(VueQrcodeReader)
-axios.defaults.baseURL = 'https://clutch-api.philliplogan.com' 
-Vue.config.productionTip = false
+Vue.use(VueQrcodeReader);
+axios.defaults.baseURL = "https://clutch-irpl.vercel.app";
+Vue.config.productionTip = false;
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
