375e61f538ef078b1389726a2981de2cd09b32eb
Phillip Logan <philo3194@gmail.com>
2022-02-10 04:03:42 +0000

    done now

--- client/src/components/Made.vue
+++ client/src/components/Made.vue
@@ -2,13 +2,15 @@
   <div>
     <h1>Your game has been made</h1>
 
-    <p>You'll use this QRCode to start your game.</p>
+    <p>You'll use this QR-Code to start your game.</p>
     <div class="made-qr">
       <img v-bind:src="'https://api.qrserver.com/v1/create-qr-code/?size=300x300&margin=35&data=' + game.event" />
     </div>
 
-    <p>Print and hide these QRCodes for your friends to find.</p>
+    <p>Print and hide these QR-Codes for your friends to find.</p>
     <div class="made-qr" v-for="(quest, index) in game.quests" :key="index">
+      <div>{{ index + 1 }}. {{ quest.clue }}</div>
+      <br />
       <img v-bind:src="'https://api.qrserver.com/v1/create-qr-code/?size=300x300&margin=35&data=' + quest._id" />
     </div>
   </div>
